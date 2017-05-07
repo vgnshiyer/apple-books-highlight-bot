a84c916a44b13a1f56913e049466211329859039
Matthew Trentacoste <web+git@matttrent.com>
2017-05-07 01:15:50 -0700

    think it's working

--- ibooks_highlights_exporter.py
+++ ibooks_highlights_exporter.py
@@ -7,10 +7,12 @@ import datetime
 import argparse
 import re
 import frontmatter
+import datetime as dt
 
 from glob import glob
 from jinja2 import Environment, FileSystemLoader
 from slugify import slugify
+from dateutil import parser as duparser
 
 
 PATH = os.path.dirname(os.path.abspath(__file__))
@@ -19,6 +21,9 @@ TEMPLATE_ENVIRONMENT = Environment(
     loader=FileSystemLoader(os.path.join(PATH, 'templates')),
     trim_blocks=True,
     lstrip_blocks=False)
+
+
+NS_TIME_INTERVAL_SINCE_1970 = 978307200
 
 
 ANNOTATION_DB_PATH = (
@@ -57,11 +62,12 @@ NOTE_LIST_FIELDS = [
     'represent_text', 
     'chapter', 
     'style', 
+    'modified_date'
 ]
 
 NOTE_LIST_QUERY = """
 select 
-ZANNOTATIONASSETID as assetid, 
+ZANNOTATIONASSETID as asset_id, 
 books.ZBKLIBRARYASSET.ZTITLE as title, 
 books.ZBKLIBRARYASSET.ZAUTHOR as author,
 ZANNOTATIONLOCATION as location,
@@ -69,7 +75,8 @@ ZANNOTATIONSELECTEDTEXT as selected_text,
 ZANNOTATIONNOTE as note,
 ZANNOTATIONREPRESENTATIVETEXT as represent_text, 
 ZFUTUREPROOFING5 as chapter, 
-ZANNOTATIONSTYLE as style
+ZANNOTATIONSTYLE as style,
+ZANNOTATIONMODIFICATIONDATE as modified_date
 
 from ZAEANNOTATION
 
@@ -191,15 +198,9 @@ def cmp_to_key(mycmp):
 class Annotation(object):
 
     def __init__(self, location, selected_text=None, note=None,
-        represent_text=None, chapter=None, style=None):
+        represent_text=None, chapter=None, style=None, modified_date=None):
 
         if (selected_text is None) and (note is None):
-            print(location)
-            print(selected_text)
-            print(note)
-            print(represent_text)
-            print(chapter)
-            print(style)
             raise ValueError('specify either selected_text or note')
 
         self.location = location
@@ -212,6 +213,7 @@ class Annotation(object):
         self.chapter = chapter
         self.style = style
         self.note = note
+        self.modified_date = modified_date
 
     def __getitem__(self, key):
         return getattr(self, key)
@@ -227,36 +229,82 @@ class Book(object):
         if args_present == file_present:
             raise ValueError('specify either asset_id or filename')
 
+        self._modified_date = None
         self._annotations = []
 
         if args_present:
             self._asset_id = asset_id
-            self.author = None
-            self.title = None
+            self._author = None
+            self._title = None
             self._prev_content = None
 
         if file_present:
             self._process_file(filename)
 
     def _process_file(self, filename):
+
+        self._filename = os.path.split(filename)[-1]
+
         book = frontmatter.load(filename)
         self._asset_id = book['asset_id']
-        self.author = book['author']
-        self.title = book['title']
+        self._author = book['author']
+        self._title = book['title']
+
+        if 'modified_date' in book.keys():
+            self._modified_date = duparser.parse(book['modified_date'])
 
         self._prev_content = book.content
 
     def __str__(self):
-        return '{asset_id} {len}\t{title}, {author}'.format(
+        mod = ' '
+        if self.is_modified:
+            mod = '*'
+        return '{asset_id} {mod} {len}\t{title}, {author}'.format(
             asset_id=self._asset_id.ljust(32),
+            mod=mod,
             len=self.num_annotations, 
-            title=self.title,
-            author=self.author
+            title=self._title,
+            author=self._author
         )
+
+    def _yaml_str(cls, txt):
+        exp = '[^A-Za-z0-9 ]+'
+        return re.sub(exp, '', txt)
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        self._author = self._yaml_str(value)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = self._yaml_str(value)
+        self._filename = slugify(value) + '.md'
 
     @property
     def asset_id(self):
         return self._asset_id
+
+    @property
+    def is_modified(self):
+        if self._modified_date is None:
+            if self.num_annotations > 0:
+                return True
+            else:
+                return False
+
+        anno_max = max([
+            anno.modified_date
+            for anno in self._annotations
+        ])
+        return anno_max > self._modified_date
 
     @property
     def annotations(self):
@@ -282,9 +330,29 @@ class Book(object):
         md = template.render(
             title=self._title,
             author=self._author,
-            highlights=book,
+            highlights=self.annotations,
         )
         return md
+
+    def write(self, path):
+
+        mod_date = max([
+            anno.modified_date
+            for anno in self._annotations
+        ])
+        mod_date = mod_date.isoformat()
+
+        fmpost = frontmatter.Post(
+            self.content, 
+            asset_id=self._asset_id,
+            title=self.title,
+            author=self.author,
+            modified_date=mod_date
+        )
+
+        fn = os.path.join(path, self._filename)
+        with open(fn, 'w') as f:
+            frontmatter.dump(fmpost, f)
 
 
 class BookList(object):
@@ -308,13 +376,63 @@ class BookList(object):
 
         return md_books
 
-    def get_create_book(self, asset_id):
+    def _get_create_book(self, asset_id):
 
         if asset_id not in self.books:
             book = Book(asset_id=asset_id)
             self.books[asset_id] = book
 
         return self.books[asset_id]
+
+    def populate_annotations(self, annos):
+
+        res = [
+            r
+            for r in annos
+            if r['asset_id'] is not None and
+            ((r['selected_text'] is not None) or (r['note'] is not None))
+        ]
+
+        for r in res:
+            book = self._get_create_book(r['asset_id'])
+            if book.title is None:
+                book.title = r['title']
+            if book.author is None:
+                book.author = r['author']
+
+        anno_group = {}
+        for r in res:
+            asset_id = r['asset_id']
+            if asset_id not in anno_group:
+                anno_group[asset_id] = []
+
+            anno = Annotation(
+                location=r['location'], 
+                selected_text=r['selected_text'],
+                note=r['note'],
+                represent_text=r['represent_text'],
+                chapter=r['chapter'],
+                style=r['style'],
+                modified_date=dt.datetime.fromtimestamp(
+                    NS_TIME_INTERVAL_SINCE_1970 + r['modified_date']),
+            )
+            anno_group[asset_id].append(anno)
+
+        for asset_id, anno in anno_group.items():
+            self.books[asset_id].annotations = anno        
+
+    def write_modified(self, path=None):
+
+        if path == None:
+            path = self._path
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        for book in self.books.values():
+            if not book.is_modified:
+               continue
+            book.write(path)
 
 
 def load_markdown_books(args):
@@ -330,68 +448,25 @@ def load_markdown_books(args):
     return md_books
 
 
-def print_book_list(args):
-
-    book_list = BookList(args.dname)
-
-    # only prints a list of books with highlights and exists
-    print('=== ibooks database ===')
-    cur = get_ibooks_database()
-    res = cur.execute(BOOK_LIST_QUERY)
-    res = sorted(res, key=lambda x: x[1])
-    for assetid, count, title, author in res:
-        if count > 0:
-            print(assetid.ljust(32), count, '\t', title, ',', author)
-
-    print('\n\n=== filesystem ===')
-    for book in book_list.books.values():
-        print(book)
-
-
-def print_book_list_2(args):
-
-    book_list = BookList(args.dname)
+def fetch_annotations():
 
     cur = get_ibooks_database()
     res = cur.execute(NOTE_LIST_QUERY)
     res = res.fetchall()
     res = [dict(zip(NOTE_LIST_FIELDS, r)) for r in res]
 
-    res = [
-        r
-        for r in res
-        if r['asset_id'] is not None and
-        ((r['selected_text'] is not None) or (r['note'] is not None))
-    ]
+    return res
 
-    for r in res:
-        book = book_list.get_create_book(r['asset_id'])
-        if book.title is None:
-            book.title = r['title']
-        if book.author is None:
-            book.author = r['author']
 
-    anno_group = {}
-    for r in res:
-        asset_id = r['asset_id']
-        if asset_id not in anno_group:
-            anno_group[asset_id] = []
-
-        anno = Annotation(
-            location=r['location'], 
-            selected_text=r['selected_text'],
-            note=r['note'],
-            represent_text=r['represent_text'],
-            chapter=r['chapter'],
-            style=r['style']
-        )
-        anno_group[asset_id].append(anno)
-
-    for asset_id, anno in anno_group.items():
-        book_list.books[asset_id].annotations = anno
+def print_book_list(book_list):
 
     for book in book_list.books.values():
         print(book)
+        print(book.is_modified)
+
+
+def write_book_notes(path):
+    book_list.write_modified(path)
 
 
 def do_note_list(args):
@@ -452,11 +527,11 @@ if __name__ == '__main__':
     parser.add_argument('--list', action="store_true", help="Lists a books having highlights.")
     args = parser.parse_args()
 
+    book_list = BookList(args.dname)
+    annos = fetch_annotations()
+    book_list.populate_annotations(annos)
 
     if args.list:
-        print_book_list_2(args)
-
+        print_book_list(book_list)
     else:
-        if not os.path.exists(args.dname):
-            os.makedirs(args.dname)
-        do_note_list(args)
+        write_book_notes(args.dname)
