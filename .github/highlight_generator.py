69a5a76a8522fa5c7099cd0c889e2594d979d963
vgnshiyer <vgnshiyer@gmail.com>
2024-01-26 11:01:25 -0700

    chore: modified github actions

--- .github/highlight_generator.py
+++ .github/highlight_generator.py
@@ -1,19 +1,24 @@
-import pathlib, json, logging, random, datetime
+import pathlib, json, logging, random, datetime, os
 from jinja2 import Template
+
+# Create logs directory if it doesn't exist
+pathlib.Path('logs').mkdir(parents=True, exist_ok=True)
+
+print(os.listdir())
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s %(name)s %(message)s",
     level=logging.DEBUG,
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler("../logs/highlight_generator.log", mode="w")
+        logging.FileHandler("logs/highlight_generator.log", mode="w")
     ]
 )
 logger = logging.getLogger(__name__)
 
 def get_books():
     books = []
-    for book in pathlib.Path("../highlights_data").iterdir():
+    for book in pathlib.Path("highlights_data").iterdir():
         if book.is_file() and book.suffix == ".json":
             books.append(book)
     logger.info(f"Found {len(books)} books")
@@ -45,7 +50,7 @@ def generate_daily_highlights_email():
     book_data = get_book_data(book)
     
     logger.info(f"Generating email for {book}")
-    with open("./templates/email_template.html", "r") as f:
+    with open(".github/templates/email_template.html", "r") as f:
         template = Template(f.read())
     return template.render(
         book_title=book_data["title"],
@@ -61,6 +66,6 @@ if __name__ == "__main__":
     email = generate_daily_highlights_email()
     logger.info("Email generated")
 
-    with open("./output.html", "w") as f:
+    with open(".github/output.html", "w") as f:
         f.write(email)
     logger.info("Email written to file")
